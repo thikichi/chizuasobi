1f354301285bc87af796214ad858d40487608911
hikichi <t.hikichi@live.jp>
2019-10-03 12:50:37 +0900

    修正

--- page-test2.php
+++ page-test2.php
@@ -9,6 +9,12 @@
 
 
 <?php
+$distance = 10;
+// 東京タワー 座標(WGS84)　緯度: 35.658581 経度: 139.745433
+// 東京駅 座標(WGS84)　緯度: 35.681236 経度: 139.767125
+$lat1 = 35.658581;
+$lng1 = 139.745433;
+
 $landmark_posts  = get_posts( array( 'post_type'=>'landmark', 'numberposts'=>-1 ) );
 $marker_data_arr = array();
 $i=0;
@@ -16,14 +22,30 @@ foreach ($landmark_posts as $landmark_post) {
     $map_center = get_post_meta( $landmark_post->ID, 'acf_landmark_gmap', true );
     $map_zoom   = get_post_meta( $landmark_post->ID, 'acf_landmark_zoom', true );
     $land_cat   = get_the_terms( $landmark_post->ID, 'landmark_cateogry' );
+    // 起点となる史跡と距離を比較
+    
+    // $dist = distance($lat1, $lng1, $map_center['lat'], $map_center['lng'], true);
+    // if( $dist < $distance ) {
+    //   // get markerData
+    //   $marker_data_arr[$i]['name'] = $landmark_post->post_title;
+    //   $marker_data_arr[$i]['lat']  = $map_center['lat'];
+    //   $marker_data_arr[$i]['lng']  = $map_center['lng'];
+    //   $marker_data_arr[$i]['cat']  = $land_cat[0]->term_id;
+    //   $i++;
+    // }
+    $dist = distance($lat1, $lng1, $map_center['lat'], $map_center['lng'], true);
+
     // get markerData
+    $marker_data_arr[$i]['id']   = $landmark_post->ID;
     $marker_data_arr[$i]['name'] = $landmark_post->post_title;
     $marker_data_arr[$i]['lat']  = $map_center['lat'];
     $marker_data_arr[$i]['lng']  = $map_center['lng'];
     $marker_data_arr[$i]['cat']  = $land_cat[0]->term_id;
+    $marker_data_arr[$i]['dist'] = floor($dist);
     $i++;
 }
-// var_dump($marker_data_arr);
+
+
 ?>
 
 <div id="map" style="width: 100%;height: 500px"></div>
@@ -34,7 +56,12 @@ foreach ($all_land_cats as $all_land_cat): ?>
   <input id="MarkerCheck-<?php echo esc_attr($all_land_cat->term_id); ?>" type="checkbox" name="marker-check-1" value="<?php echo esc_attr($all_land_cat->term_id); ?>">
   <?php echo esc_html($all_land_cat->name); ?>
 <?php endforeach; ?>
-
+<select id="MarkerSelectDist">
+  <option value="10000" selected>10000m以下</option>
+  <option value="7500"> 7500m以下</option>
+  <option value="5000"> 5000m以下</option>
+  <option value="3000"> 3000m以下</option>
+</select>
 
 <script>
 jQuery(function($) {
@@ -44,10 +71,12 @@ var infoWindow = [];
 var markerData = [ // マーカーを立てる場所名・緯度・経度
 <?php foreach ($marker_data_arr as $marker_data): ?>
  {
+    id:   <?php echo esc_js($marker_data['id']); ?>,
     name: '<?php echo esc_js($marker_data['name']); ?>',
-    lat: <?php echo esc_js($marker_data['lat']); ?>,
-    lng: <?php echo esc_js($marker_data['lng']); ?>,
-    cat: <?php echo esc_js($marker_data['cat']); ?>,
+    lat:  <?php echo esc_js($marker_data['lat']); ?>,
+    lng:  <?php echo esc_js($marker_data['lng']); ?>,
+    cat:  <?php echo esc_js($marker_data['cat']); ?>,
+    dist: <?php echo esc_js($marker_data['dist']); ?>,
  },
 <?php endforeach; ?>
 ];
@@ -72,7 +101,6 @@ function markerEvent(i) {
 // マーカー毎の処理
 function dispMarker(markerData, catNum) {
   for (var i = 0; i < markerData.length; i++) {
-
       if( markerData[i]['cat']===catNum ) {
         markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']});
         marker[i] = new google.maps.Marker({ // マーカーの追加
@@ -86,15 +114,28 @@ function dispMarker(markerData, catNum) {
        markerEvent(i); // マーカーにクリックイベントを追加
      } 
   }
+  // hiddenMakersAll( markerData, 3000 );
 }
 
 //マーカーを削除する
 function deleteMakers(markerData, catNum) {
   for (var i = 0; i < markerData.length; i++) {
-    if( markerData[i]['cat']===catNum ) {
+    if( markerData[i]['cat']===catNum) {
       marker[i].setMap(null);
     }
   }
+}
+
+function hiddenMakersAll( markerData, dist=3000 ) {
+  $.each(marker, function(index, val) {
+    if(marker[index]) {
+      if( markerData[index]['dist'] < dist) {
+        marker[index].setVisible(true);
+      } else {
+        marker[index].setVisible(false);
+      }
+    }
+  });
 }
 
 initMap();
@@ -109,6 +150,13 @@ $(function() {
       }
     });
   <?php endforeach; ?>
+
+  $('#MarkerSelectDist').change(function() {
+    //選択したvalue値を変数に格納
+    var val = $(this).val();
+    hiddenMakersAll( markerData, val );
+  });
+
 });
 
 });
@@ -119,6 +167,7 @@ $(function() {
 
 
 <?php
+$distance = 10; // 距離 km
 // 東京タワー 座標(WGS84)　緯度: 35.658581 経度: 139.745433
 $lat1 = 35.681236;  // 緯度
 $lng1 = 139.767125; // 経度
@@ -134,16 +183,19 @@ foreach ($landmark_posts as $landmark_post) {
 
   // 起点となる史跡と距離を比較
   $dist = distance($lat1, $lng1, $map_center['lat'], $map_center['lng'], true);
-  if( $dist < 10 ) {
+  if( $dist < $distance ) {
     // get markerData
     $marker_data_arr[$i]['name'] = $landmark_post->post_title;
     $marker_data_arr[$i]['lat']  = $map_center['lat'];
     $marker_data_arr[$i]['lng']  = $map_center['lng'];
     $marker_data_arr[$i]['cat']  = $land_cat[0]->term_id;
     $i++;
+  } else {
+
   }
 }
 var_dump($marker_data_arr);
+
 
 ?>
 
