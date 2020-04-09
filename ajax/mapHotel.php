9cf9fde08deb49c76932a02dcd279ce291f9c8ed
thikichi <t.hikichi@live.jp>
2020-04-09 12:40:58 +0900

    ホテル一覧のajax関連ファイル

---
+++ ajax/mapHotel.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Google Map Ajax
+*/
+function mapHotelFunc(){
+  global $osfw;
+  $returnObj  = array();
+  $mapid     = $_POST['mapid'];
+  $marker_size = $_POST['marker_size'];
+  $post_id = $_POST['post_id'];
+
+  $acf_landmark_gmap = get_post_meta( $post_id, 'acf_landmark_gmap', true );
+  $lat_main = $acf_landmark_gmap['lat']; // 経度
+  $lng_main = $acf_landmark_gmap['lng']; // 緯度
+  $jx = ceil(($lng_main * 1.000106961 - $lat_main * 0.000017467 - 0.004602017) * 3600000);
+  $jy = ceil(($lat_main * 1.000083049 + $lng_main * 0.000046047 - 0.010041046) * 3600000);
+  $url = "http://jws.jalan.net/APIAdvance/HotelSearch/V1/?key=leo16d0c4beac1&x=" . $jx . "&y=" . $jy . "&range=50";
+  $xml = @simplexml_load_file($url);
+  // 中心の史跡の座標
+  $returnObj['mapcenter']['lat'] = $lat_main;
+  $returnObj['mapcenter']['lng'] = $lng_main;
+  // マーカー画像
+  $img_url = get_landmark_thumbnail( $post_id );
+  $title   = esc_html(get_the_title( $post_id ));
+  $address = esc_html(get_post_meta( $post_id, 'acf_landmark_address', true ));
+  $link    = esc_url(get_the_permalink( $post_id ));
+  // 中心のマーカーオブジェクト
+  $returnObj['markerDataAjax'][0]['id']   = $mapid . "_" . 0;
+  $returnObj['markerDataAjax'][0]['name'] = get_the_title( $post_id );
+  $returnObj['markerDataAjax'][0]['lat']  = $lat_main;
+  $returnObj['markerDataAjax'][0]['lng']  = $lng_main;
+  $returnObj['markerDataAjax'][0]['cat_icon'] = $marker['src'];
+  $returnObj['markerDataAjax'][0]['infoWindowContent'] = get_infowindow_tag($mapid, $img_url, $title, $address, $link );
+
+  $i=1;
+  $tag = '';
+  foreach ($xml->Hotel as $hotel) {
+    // 日本測地系から世界測地系の緯度・経度に変更
+    $rdata = change_geodetic_system_from_jp_to_wd( $hotel->Y, $hotel->X );
+    $lat = $rdata['lat'];
+    $lng = $rdata['lng'];
+    // photo
+    $img_url = $hotel->PictureURL;
+    $args = array(
+      'title'     => $hotel->HotelName,
+      'image'     => esc_url($img_url),
+      'text'      => $hotel->HotelCaption,
+      'link_map'  => 'javascript:mapHotelClick(\'' . $mapid . '_' . $i . '\')',
+      'link_main' => '#mapHotel_' . $i,
+      'id'        => '#mapHotel_' . $i,
+      'link_text' => 'ホテルの詳細へ',
+    );
+    $tag .= slider_tags( $args );
+
+    // マーカーオブジェクトをつくる
+    $returnObj['markerDataAjax'][$i]['id']   = $mapid . "_" . $i;
+    $returnObj['markerDataAjax'][$i]['name'] = $hotel->HotelName;
+    $returnObj['markerDataAjax'][$i]['lat']  = $lat;
+    $returnObj['markerDataAjax'][$i]['lng']  = $lng;
+    $returnObj['markerDataAjax'][$i]['cat_icon'] = $marker['src'];
+    $returnObj['markerDataAjax'][$i]['infoWindowContent'] = get_infowindow_tag($mapid, $hotel->PictureURL, $hotel->HotelName, $hotel->HotelAddress, $hotel->HotelDetailURL );
+    $i++;
+  }
+  $returnObj['tags'] = $tag;
+
+  echo json_encode( $returnObj );
+  die();
+}
+add_action( 'wp_ajax_mapHotelFunc', 'mapHotelFunc' );
+add_action( 'wp_ajax_nopriv_mapHotelFunc', 'mapHotelFunc' );
---
+++ parts-js/mapHotel.php
@@ -0,0 +1,95 @@
+<?php
+$gmap = get_post_meta( $post->ID, 'acf_landmark_gmap', true );
+$zoom = get_post_meta( $post->ID, 'relationplace_zoom', true );
+?>
+<script>
+var mapHotelMarker = [];
+jQuery(function($) {
+  $(function(){
+    var markerSize ='img_marker_large';
+
+    // 遅延読み込み部分
+    var mapHotelDone = function() {
+
+      var markerData = [];
+      $.ajax({
+          type: 'POST',
+          url: ajaxurl,
+          data: {
+            'action'  : 'mapHotelFunc',
+            'mapid'   : 'mapHotel',
+            'marker_size' : markerSize,
+            'post_id' : <?php echo $post->ID; ?>
+          },
+          success: function( response ){
+            jsonData = JSON.parse( response );
+            console.log(jsonData);
+            markerData = jsonData['markerDataAjax'];
+            // var last_lat = markerData[markerData.length - 1]['lat'];
+            // var last_lng = markerData[markerData.length - 1]['lng'];
+            var mapLatLng = getCenerLatLng( jsonData['mapcenter']['lat'], jsonData['mapcenter']['lng'] );
+            var map = initMap( 'mapHotel', mapLatLng, <?php echo $zoom; ?> );
+            mapHotelMarker = dispMarker2( map, markerData );
+            $('#mapHotelUL').html(jsonData['tags']).promise().done(function(){
+              //your callback logic / code here
+              $('.layout3-slider').slick({
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                prevArrow: '<a href="javascript:void(0)" class="slide-arrow prev-arrow"><span class="_inner"><svg class="icon-svg-arrow" version="1.1" id="レイヤー_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="15px" height="27.5px" viewBox="0 0 60 110" enable-background="new 0 0 60 110" xml:space="preserve"><polyline fill="none" stroke-width="5" stroke-miterlimit="10" points="55.892,105.002 5.892,55.002 55.892,5.002"/></svg></span></a>',
+                nextArrow: '<a href="javascript:void(0)" class="slide-arrow next-arrow"><span class="_inner"><svg class="icon-svg-arrow" version="1.1" id="レイヤー_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="15px" height="27.5px" viewBox="0 0 60 110" enable-background="new 0 0 60 110" xml:space="preserve"><polyline fill="none" stroke-width="5" stroke-miterlimit="10" points="3.892,5.002 53.892,55.002 3.892,105.002 "/></svg></span></a>',
+                responsive: [
+                  {
+                    breakpoint: 991,
+                    settings: {
+                      slidesToShow: 4,
+                      centerMode: false,
+                    }
+                  },
+                  {
+                    breakpoint: 767,
+                    settings: {
+                      slidesToShow: 3,
+                      centerMode: true,
+                    }
+                  },
+                  {
+                    breakpoint: 575,
+                    settings: {
+                      slidesToShow: 2,
+                      centerMode: true,
+                    }
+                  },
+                  {
+                    breakpoint: 400,
+                    settings: {
+                      slidesToShow: 1,
+                      centerMode: true,
+                    }
+                  }
+                ],
+              });
+            });
+
+
+
+
+
+          }
+      });
+    }
+    $('#mapHotel').myLazyLoadingObj({
+      callback : mapHotelDone,
+    });
+
+    $('input[name="chgmarkerMSF"]').change(function() {
+      markerSize = $("input[name='chgmarkerMSF']:checked").val();
+      mapHotelDone();
+    });
+
+  });
+});
+function mapHotelClick( linkid ) {
+  google.maps.event.trigger(mapHotelMarker[linkid], "click");
+  document.getElementById('mapHotelWrap').scrollIntoView({behavior: 'smooth', block: 'start'});
+}
+</script>
